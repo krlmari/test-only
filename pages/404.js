488dc42b9ee5be397150d7ce648da8812bdb0986
krlmari <krylova_m_v@inbox.ru>
2022-02-12 20:43:25 +0300

    update error

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { CenterBlock } from "../styles/CenterBlock.styled";
+
 const Error = () => {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/");
+        }, 3000);
+    }, [router]);
+
     return (
         <>
-            <h1>404</h1>
-            <h2>Something is going wrong...</h2>
+            <CenterBlock>
+                <h1>404</h1>
+                <h2>Something is going wrong...</h2>
+            </CenterBlock>
         </>
     );
 };
---
+++ styles/CenterBlock.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const CenterBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 40vh;
+    text-align: center;
+    h1,
+    h2 {
+        font-weight: normal;
+    }
+`;
